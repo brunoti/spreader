94f3376cf681e6d09c51013bbee2a1d3b97aeecb
BruNo Oliveira <bruno.oliveirati1@gmail.com>
2016-12-06 09:00:05 -0200

    Remove the custom construct on DeviceCollection
    - Many methods on Illuminate\Support\Collection uses a new static($itens) to
      keep the processed data inside a collection.
    - So i can not override the construct method.

--- src/Collections/DeviceCollection.php
+++ src/Collections/DeviceCollection.php
@@ -8,31 +8,6 @@ use Illuminate\Support\Collection;
 class DeviceCollection extends Collection
 {
     /**
-     * DeviceCollection constructor
-     *
-     * @param Array<Device> $devices
-     */
-    public function __construct(array $devices = [])
-    {
-        $devices = is_array($devices) ? $devices : func_get_args();
-
-        array_walk($devices, [$this, 'add']);
-    }
-
-    /**
-     * Adds a device to the collection
-     *
-     * @param Device $device
-     *
-     * @return DeviceCollection
-     */
-    public function add(Device $device)
-    {
-        $this->push($device);
-        return $this;
-    }
-
-    /**
      * Return an array of unique tokens
      *
      * @return Collection
